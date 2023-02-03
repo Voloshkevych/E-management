371ac6303901893ef3ec192e45f69e48ca597a2b
Voloshkevych <voloshkevych.b@gmail.com>
2023-02-03 22:28:07 +0200

    Ready to go

---
+++ src/main/java/com/petproject/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.petproject.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.petproject.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+	public List<Employee> searchBy(String theName);
+	
+}
